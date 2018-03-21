bada494501effb56461d9d183a419428e90eebb5
vigneshpandi08 <37330800+vigneshpandi08@users.noreply.github.com>
2018-03-21 14:17:48 +0530

    Added Class Libraries and MVC application

---
+++ MyShop1/MyShop1.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyShop1.WebUI.MvcApplication" Language="C#" %>
---
+++ MyShop1/MyShop1.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyShop1.WebUI.Startup))]
+namespace MyShop1.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
