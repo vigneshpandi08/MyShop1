12c9167812fb953f5f9480084654a6820f7473f8
vigneshpandi08 <37330800+vigneshpandi08@users.noreply.github.com>
2018-03-21 14:22:25 +0530

    add model

---
+++ MyShop1/MyShop1.Core/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop1.Core
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }
+        public Product()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }
+}
