0570628612d699d4ba1cae7e03c078844813be42
vigneshpandi08 <37330800+vigneshpandi08@users.noreply.github.com>
2018-03-23 10:29:39 +0530

    Add images successfully

---
+++ MyShop1/MyShop1.DataAccess.SQL/SQLRepository.cs
@@ -0,0 +1,62 @@
+﻿using MyShop1.Core.Contracts;
+using MyShop1.Core.Models;
+using MyShop1.DataAccess.SQL;
+using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class SQLRepository<T> : IRepo<T> where T : BaseEntity
+    {
+        internal DataContext context;
+        internal DbSet<T> dbSet;
+
+        public SQLRepository(DataContext context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<T>();
+        }
+
+
+        public IQueryable<T> Collection()
+        {
+            return dbSet;
+        }
+
+        public void Commit()
+        {
+            context.SaveChanges();
+        }
+
+        public void Delete(string Id)
+        {
+            var t = Find(Id);
+            if (context.Entry(t).State == EntityState.Detached)
+                dbSet.Attach(t);
+
+            dbSet.Remove(t);
+        }
+
+        public void DoSomething()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Find(string Id)
+        {
+            return dbSet.Find(Id);
+        }
+
+        public void Insert(T t)
+        {
+            dbSet.Add(t);
+        }
+
+        public void Update(T t)
+        {
+            dbSet.Attach(t);
+            context.Entry(t).State = EntityState.Modified;
+        }
+    }
+}
